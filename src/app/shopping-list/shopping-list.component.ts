7364ca7e5facc39797929759378d1c6fd882b553
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2024-09-12 13:09:35 +0530

    Using Subject Observable

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,25 +1,31 @@
 import { ShoppingListService } from './shopping-list.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Ingredients } from '../Shared/ingredient.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
-export class ShoppingListComponent implements OnInit{
+export class ShoppingListComponent implements OnInit, OnDestroy{
 
   ingredients:Ingredients[]=[];
+  igChangedSub:Subscription;
 
   constructor(private shoppingListService:ShoppingListService){}
 
   ngOnInit(): void {
     this.ingredients=this.shoppingListService.getIngredient();
-    this.shoppingListService.ingredientChanged.subscribe(
+    this.igChangedSub=this.shoppingListService.ingredientChanged.subscribe(
       (ingredients:Ingredients[])=>{
         this.ingredients=ingredients;
       }
     )
+  }
+
+  ngOnDestroy(): void {
+    this.igChangedSub.unsubscribe();
   }
   
 }
