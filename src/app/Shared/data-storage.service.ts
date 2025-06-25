60bf9abfdcff746a6dbb16a5f8068f0fb138fecf
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2025-06-25 20:59:11 +0530

    fetching recipes from firebase DB

--- src/app/Shared/data-storage.service.ts
+++ src/app/Shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
 
 @Injectable({providedIn:"root"})
 
@@ -10,5 +11,9 @@ export class DataStorageService{
     storeRecipes(){
         const recipes= this.recipeService.getRecipes();
         this.http.put('https://recipe-book-11c8e-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe(res=>{console.log(res)});
+    }
+
+    fetchRecipes(){
+        return this.http.get<Recipe[]>('https://recipe-book-11c8e-default-rtdb.firebaseio.com/recipes.json').subscribe(res=>this.recipeService.setRecipes(res))        
     }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,52 +6,53 @@ import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class RecipeService {
-    recipesChanged= new Subject<Recipe[]>();
-    private recipes: Recipe[] = [
-        new Recipe(
-         'test',
-         'test desc',
-         'https://cdn.pixabay.com/photo/2015/06/01/23/43/pasta-794464_1280.jpg',
-         [ new Ingredient('meat',1),
-            new Ingredient('French fries',10)
-         ]
-        ),
-        new Recipe(
-            'test2',
-            'test desc2',
-            'https://cdn.pixabay.com/photo/2015/06/01/23/43/pasta-794464_1280.jpg',
-            [ new Ingredient('meat',5),
-                new Ingredient('French fries',2)
-             ]
-           )
-     ];
+  recipesChanged = new Subject<Recipe[]>();
+  private recipes: Recipe[] = [
+    new Recipe(
+      'test',
+      'test desc',
+      'https://cdn.pixabay.com/photo/2015/06/01/23/43/pasta-794464_1280.jpg',
+      [new Ingredient('meat', 1), new Ingredient('French fries', 10)]
+    ),
+    new Recipe(
+      'test2',
+      'test desc2',
+      'https://cdn.pixabay.com/photo/2015/06/01/23/43/pasta-794464_1280.jpg',
+      [new Ingredient('meat', 5), new Ingredient('French fries', 2)]
+    ),
+  ];
 
-     constructor(private slSer:ShoppingListService){}
+  constructor(private slSer: ShoppingListService) {}
 
-     getRecipes() {
-        return this.recipes.slice();
-    }
+  setRecipes(recipes:Recipe[]){
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice())
+  }
 
-    getRecipe(index:number){
-        return this.recipes[index];
-    }
+  getRecipes() {
+    return this.recipes.slice();
+  }
 
-    addIngredientsToShoppingList(ings:Ingredient[]){
-        this.slSer.addIngredients(ings);
-    }
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
 
-    addRecipe(newRecipe:Recipe){
-        this.recipes.push(newRecipe);
-        this.recipesChanged.next(this.recipes.slice())
-    }
-    
-    updateRecipe(index:number, newRecipe:Recipe){
-        this.recipes[index]= newRecipe;
-        this.recipesChanged.next(this.recipes.slice());
-    }
-    
-    deleteRecipe(index:number){
-        this.recipes.splice(index,1);
-        this.recipesChanged.next(this.recipes.slice());
-    }
+  addIngredientsToShoppingList(ings: Ingredient[]) {
+    this.slSer.addIngredients(ings);
+  }
+
+  addRecipe(newRecipe: Recipe) {
+    this.recipes.push(newRecipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
 }
