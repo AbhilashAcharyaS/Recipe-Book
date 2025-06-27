49d6c3f1f8b6887f53c0cf1ef6bac1f375440e5c
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2025-06-27 13:46:38 +0530

    creating and storing user date

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,51 +1,105 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { User } from './user.model';
 
 export interface AuthResponseData {
-    kind:string,
-    idToken:string,
-    email:string,
-    refreshToken:string,
-    expiresIn:string,
-    localId:string,
-    registered?:boolean;
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   constructor(private http: HttpClient) {}
+  user = new Subject<User>();
 
   signUpUser(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=',
-      { email: email, password: password, returnSecureToken: true }
-    ).pipe(catchError(this.handleError))
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=',
+        { email: email, password: password, returnSecureToken: true }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
   }
 
-  login(email:string,password:string){
-    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=',
-    {email: email, password: password, returnSecureToken: true})
-    .pipe(catchError(this.handleError))
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=',
+        { email: email, password: password, returnSecureToken: true }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
   }
 
-  private handleError(errorRes:HttpErrorResponse){
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userId, token, expirationDate);
+    this.user.next(user);
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
     console.log(errorRes);
-    
-    let errorMsg="An unknown error occured!"
-        if(!errorRes.error || !errorRes.error.error){
-            return throwError(errorMsg);
-        }
-        switch (errorRes.error.error.message){
-            case "EMAIL_EXISTS": errorMsg="The email address is already in use by another account.";break;
-            case "OPERATION_NOT_ALLOWED": errorMsg="Password sign-in is disabled for this project";break;
-            case "TOO_MANY_ATTEMPTS_TRY_LATER": errorMsg="We have blocked all requests from this device due to unusual activity. Try again later"; break;
-            case "EMAIL_NOT_FOUND": errorMsg="There is no user record corresponding to this identifier";break;
-            case "INVALID_PASSWORD": errorMsg="The password is invalid or the user does not have a password";break;
-            case "USER_DISABLED": errorMsg="The user account has been disabled by an administrator"; break;
-            case "INVALID_LOGIN_CREDENTIALS": errorMsg="Invalid login credentials"; break;
-        }
-        return throwError(errorMsg);
+
+    let errorMsg = 'An unknown error occured!';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMsg);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMsg = 'The email address is already in use by another account.';
+        break;
+      case 'OPERATION_NOT_ALLOWED':
+        errorMsg = 'Password sign-in is disabled for this project';
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMsg =
+          'We have blocked all requests from this device due to unusual activity. Try again later';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMsg = 'There is no user record corresponding to this identifier';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMsg =
+          'The password is invalid or the user does not have a password';
+        break;
+      case 'USER_DISABLED':
+        errorMsg = 'The user account has been disabled by an administrator';
+        break;
+      case 'INVALID_LOGIN_CREDENTIALS':
+        errorMsg = 'Invalid login credentials';
+        break;
+    }
+    return throwError(errorMsg);
   }
 }
---
+++ src/app/auth/user.model.ts
@@ -0,0 +1,10 @@
+export class User{
+    constructor(public email:string, public id:string, private _token:string, private _tokenExpiryDate: Date){}
+
+    get token(){
+        if(!this._tokenExpiryDate || this._tokenExpiryDate< new Date()){
+            return null;
+        }
+        return this._token;
+    }
+}
