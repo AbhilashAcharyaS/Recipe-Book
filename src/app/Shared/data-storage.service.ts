ebfdd94e8f962a277a7e282ab1bb75593e3410b7
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2025-06-27 17:57:10 +0530

    adding token with interceptor

--- src/app/Shared/data-storage.service.ts
+++ src/app/Shared/data-storage.service.ts
@@ -1,29 +1,44 @@
 import { AuthService } from './../auth/auth.service';
-import { Injectable } from "@angular/core";
-import {HttpClient, HttpParams} from "@angular/common/http";
-import { RecipeService } from "../recipes/recipe.service";
-import { Recipe } from "../recipes/recipe.model";
-import { exhaustMap, map, take, tap } from "rxjs/operators";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { exhaustMap, map, take, tap } from 'rxjs/operators';
 
-@Injectable({providedIn:"root"})
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
-export class DataStorageService{
-    constructor(private http:HttpClient, private recipeService:RecipeService, private authService:AuthService){}
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        'https://recipe-book-11c8e-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
 
-    storeRecipes(){
-        const recipes= this.recipeService.getRecipes();
-        this.http.put('https://recipe-book-11c8e-default-rtdb.firebaseio.com/recipes.json', recipes)
-        .subscribe(res=>{console.log(res)});
-    }
-
-    fetchRecipes(){
-        return this.authService.user.pipe(take(1), exhaustMap(user=>{
-            return this.http.get<Recipe[]>('https://recipe-book-11c8e-default-rtdb.firebaseio.com/recipes.json', {
-                params:new HttpParams().set('auth', user.token)
-            })
+  fetchRecipes() {
+    return this.http
+      .get<Recipe[]>(
+        'https://recipe-book-11c8e-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map((res) => {
+          return res.map((r) => {
+            return { ...r, ingredients: r.ingredients ? r.ingredients : [] };
+          });
         }),
-        map(res=>{return res.map(r=>{return {...r, ingredients:r.ingredients?r.ingredients:[]}})}),
-        tap(recipes=>{this.recipeService.setRecipes(recipes)})       
-    )
-    }
+        tap((recipes) => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
+  }
 }
